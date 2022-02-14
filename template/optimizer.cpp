4815949b05836aa9fa6297c7ce414b9a6bf85b32
Brian Riccardi <brian.riccardi96@gmail.com>
2022-02-14 11:03:37 +0100

    Fixed arg sanitizer, added new arguments

--- template/optimizer.cpp
+++ template/optimizer.cpp
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
 #include "utils.cpp"    // ScoreManager, Rand
 using namespace std;
-constexpr array<const char*, 6> validNames = {{"a", "b", "c", "d", "e", "f"}};
-string problemName;
-int RAND_LIMIT, TIME_LIMIT;
+constexpr array<const char*, 1> ValidNames = {{"ADD-VALID-NAMES!!!"}};
+string ProblemName;
+uint64_t SEED = 0;
+size_t CANDIDATE_MOVES = 0;
+double TIME_LIMIT = oo;
+
+bool MINIMIZE = false;
 
 
-void argSanitize(int, char**);
-void readInput(string);
-void readSolution(string);
-void doMagic();
-int getScore(/*const SolutionType&*/);
-int deltaCost(/*const SolutionType&, const MoveType&*/);
-void applyMove(/*SolutionType&, const MoveType&*/);
-void printSolution(string);
+using ScoreType = int64_t; // CHANGE IF NEEDED
+
+
+
+
+void ArgSanitize(int, char**);
+void ReadInput(string);
+void ReadSolution(string);
+void DoMagic();
+ScoreType GetScore(/*const SolutionType&*/);
+ScoreType DeltaCost(/*const SolutionType&, const MoveType&*/);
+void ApplyMove(/*SolutionType&, const MoveType&*/);
+void PrintSolution(string);
 
 
 
@@ -25,38 +34,99 @@ void printSolution(string);
 int main(int argc, char** argv)
 {
     ios_base::sync_with_stdio(false);
-    argSanitize(argc, argv);
+    ArgSanitize(argc, argv);
 
-    readInput( string("input/") + problemName + ".txt" );
-    readSolution( string("output/") + problemName + ".greedy" );
-    doMagic();
-    printSolution( string("output/") + problemName + ".opt" );
+    ReadInput( string("input/") + ProblemName + ".txt" );
+    ReadSolution( string("output/") + ProblemName + ".greedy" );
+    DoMagic();
+    PrintSolution( string("output/") + ProblemName + ".opt" );
 
     return 0;
 }
 
 
-void argSanitize(int argc, char** argv)
+void ArgSanitize(int argc, char** argv)
 {
-    if(argc != 4) {
-        cerr << "Wrong number of parameters..." << endl;
-        cerr << "Usage: " << argv[0] << " <problem-name> <number-of-rand-draws> <time-limit-in-seconds>" << endl;
-        exit(1);
+    for(int i=1; i<argc; ) {
+        string arg = argv[i];
+        if(arg.substr(0, 2) == "--") {
+            arg = arg.substr(2);
+            if(arg == "problem") {
+                if(i+1 < argc) {
+                    ProblemName = argv[i+1];
+                    i += 2;
+
+                    if(find(ValidNames.begin(), ValidNames.end(), ProblemName)
+                        == ValidNames.end()
+                    ) {
+                        cerr << "Problem name is not valid!" << endl;
+                        exit(1);
+                    }
+                }else {
+                    cerr << "Missing name after `--problem` argument!" << endl;
+                    exit(1);
+                }
+            }else if(arg == "seed") {
+                if(i+1 < argc) {
+                    SEED = atoi(argv[i+1]);
+                    i += 2;
+                }else {
+                    cerr << "Missing seed after `--seed` argument!" << endl;
+                    exit(1);
+                }
+            }else if(arg == "time-limit") {
+                if(i+1 < argc) {
+                    TIME_LIMIT = atoi(argv[i+1]);
+                    i += 2;
+                }else {
+                    cerr << "Missing time-limit after `--time-limit` argument!" << endl;
+                    exit(1);
+                }
+            }else if(arg == "cand-limit") {
+                if(i+1 < argc) {
+                    CANDIDATE_MOVES = atoi(argv[i+1]);
+                    i += 2;
+                }else {
+                    cerr << "Missing number of candidates after `--cand-limit` argument!" << endl;
+                    exit(1);
+                }
+            }else if(arg == "minimize") {
+                MINIMIZE = true;
+                i += 1;
+            }else {
+                cerr << "Unknown argument --" << arg << ". Valid arguments are: " << endl;
+                cerr << "\t--problem <problem name>" << endl;
+                cerr << "\t--seed <seed value>" << endl;
+                cerr << "\t--time-limit <seconds>" << endl;
+                cerr << "\t--cand-limit <number of moves>" << endl;
+                cerr << "\t--minimize" << endl;
+                exit(1);
+            }
+        }else {
+            cerr << "Wrong form of argument. Must be pre-pended with `--`." << endl;
+            exit(1);
+        }
     }
 
-    problemName = argv[1];
-    RAND_LIMIT = atoi(argv[2]);
-    TIME_LIMIT = atoi(argv[3]);
+    if(CANDIDATE_MOVES == 0) {
+        cerr << "Warning! CANDIDATE_MOVES set to 0!" << endl;
+    }
 
-    if(find(validNames.begin(), validNames.end(), problemName.c_str()) 
-        == validNames.end()
-    ) {
-        cerr << "Problem name is not valid. Check valid name list!" << endl;
+    if(SEED == 0) {
+        cerr << "Warning! SEED set to 0!" << endl;
+    }
+
+    if(TIME_LIMIT == oo) {
+        cerr << "Warning! TIME_LIMIT set to infinity!" << endl;
+    }
+
+    if(ProblemName == "UNKNOWN") {
+        cerr << "Missing problem name. Use `--problem <problem name>` argument!" << endl;
         exit(1);
     }
 }
 
-void readInput(string filename)
+void ReadInput(string filename)
 {
     ifstream in(filename);
     if(not in) {
@@ -67,7 +137,7 @@ void readInput(string filename)
     cerr << "Reading input from " << filename << " ... " << endl;
 }
 
-void readSolution(string filename)
+void ReadSolution(string filename)
 {
     ifstream in(filename);
     if(not in) {
@@ -76,9 +146,10 @@ void readSolution(string filename)
     }
 
     // Fill "solution" variable
+    cerr << "Reading solution from " << filename << " ... " << endl;
 }
 
-void doMagic()
+void DoMagic()
 {
     cerr << "Invoking the power of the underworld..." << endl;
 
@@ -86,32 +157,32 @@ void doMagic()
     auto t1 = t0;
 
     // SolutionType current = solution;
-    int initialScore = getScore(/*current*/);
-    int bestScore = initialScore;
-    ScoreManager scoreManager(initialScore);
+    ScoreType initialScore = GetScore(/*current*/);
+    ScoreType bestScore = initialScore;
+    ScoreManager<ScoreType> scoreManager(initialScore, MINIMIZE);
 
     do {
         // Draw some number of random moves and apply the best one
 
-        vector<int> delta(RAND_LIMIT, -1);
+        vector<ScoreType> delta(CANDIDATE_MOVES, MINIMIZE ? oo : -oo);
         // vector<MoveType> candidate(RAND_LIMIT);
         #pragma omp parallel for
-        for(int r=0; r<RAND_LIMIT; ++r) {
+        for(size_t r=0; r<CANDIDATE_MOVES; ++r) {
             // MoveType mv = drawRandomMove();
-            delta[r] = deltaCost(/*current, mv*/);
+            delta[r] = DeltaCost(/*current, mv*/);
             // candidate[r] = mv;
         }
 
-        int bestDelta = 0;
+        ScoreType bestDelta = MINIMIZE ? oo : -oo;
         // MoveType bestMove = -1;
-        for(int r=0; r<RAND_LIMIT; ++r) {
-            if(delta[r] > bestDelta) {
+        for(int r=0; r<CANDIDATE_MOVES; ++r) {
+            if(delta[r] * (MINIMIZE ? -1 : +1) > bestDelta * (MINIMIZE ? -1 : +1)) {
                 bestDelta = delta[r];
                 // bestMove = candidate[r];
             }
         }
 
-        applyMove(/*current, bestMove*/);
+        ApplyMove(/*current, bestMove*/);
         scoreManager += bestDelta;
 
         if(bestDelta > 0) {
@@ -124,31 +195,31 @@ void doMagic()
 
     }while( chrono::duration<double>(t1 - t0).count() < TIME_LIMIT );
 
-    cerr << "Optimized score: " << bestScore << "\t\t[+" << (bestScore - initialScore) << "]" << endl;
+    cerr << "Optimized score: " << bestScore << "\t\t[Improvement: " << 1. * abs(bestScore - initialScore) / bestScore << "]" << endl;
 }
 
 
-int getScore(/*const SolutionType & sol*/)
+ScoreType GetScore(/*const SolutionType & sol*/)
 {
-    int score=0;
+    ScoreType score=0;
 
     return score;
 }
 
-int deltaCost(/*const SolutionType & sol, const MoveType & mv*/)
+ScoreType DeltaCost(/*const SolutionType & sol, const MoveType & mv*/)
 {
     int delta=0;
 
     return delta;
 }
 
-void applyMove(/*SolutionType & sol, const MoveType & mv*/)
+void ApplyMove(/*SolutionType & sol, const MoveType & mv*/)
 {
 
 }
 
 
-void printSolution(string filename)
+void PrintSolution(string filename)
 {
     ofstream out(filename);
     
