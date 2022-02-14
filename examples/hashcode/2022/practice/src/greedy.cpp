dc1d4a1894e812b1f36e31c4d37ce886ab0ab7f0
Brian Riccardi <brian.riccardi96@gmail.com>
2022-02-14 13:06:40 +0100

    .

--- examples/hashcode/2022/practice/src/greedy.cpp
+++ examples/hashcode/2022/practice/src/greedy.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include "utils.cpp"
 using namespace std;
-array<string, 1> ValidNames = {{"ADD-VALID-NAMES!!"}};
+array<string, 5> ValidNames = {{"a", "b", "c", "d", "e"}};
 string ProblemName = "UNKNOWN";
 uint64_t SEED = 0;
 
@@ -12,14 +12,63 @@ void Greedy();
 void PrintSolution(string);
 
 
+
+
+using SolutionType = vector<int>;
+
+
+int C, K;
+map<string, int> ingr_to_id;
+vector<string> id_to_ingr;
+
+struct Client {
+    int L, D;
+    vector<int> likes, dislikes;
+
+    void read(ifstream & cin) {
+
+        cin >> L;
+        for(int i=0; i<L; ++i) {
+            static string ingr_name;
+            cin >> ingr_name;
+            if(ingr_to_id.find(ingr_name) == ingr_to_id.end()) {
+                cerr << "New ingredient " << ingr_name << "!\n";
+                ingr_to_id[ingr_name] = id_to_ingr.size();
+                id_to_ingr.emplace_back(ingr_name);
+            }
+
+            likes.emplace_back(ingr_to_id[ingr_name]);
+        }
+
+        cin >> D;
+        for(int i=0; i<D; ++i) {
+            static string ingr_name;
+            cin >> ingr_name;
+            if(ingr_to_id.find(ingr_name) == ingr_to_id.end()) {
+                cerr << "New ingredient " << ingr_name << "!\n";
+                ingr_to_id[ingr_name] = id_to_ingr.size();
+                id_to_ingr.emplace_back(ingr_name);
+            }
+
+            dislikes.emplace_back(ingr_to_id[ingr_name]);
+        }
+    }
+};
+
+vector<Client> clients;
+
+SolutionType best;
+
+
+
 int main(int argc, char** argv)
 {
     ios_base::sync_with_stdio(false);
     ArgSanitize(argc, argv);
 
-    ReadInput( string("../input/") + ProblemName + ".txt" );
+    ReadInput( string("input/") + ProblemName + ".txt" );
     Greedy();
-    PrintSolution( string("../output/") + ProblemName + ".greedy" );
+    PrintSolution( string("output/") + ProblemName + ".greedy" );
 
     return 0;
 }
@@ -84,11 +133,27 @@ void ReadInput(string filename)
     }
 
     cerr << "Reading input from " << filename << " ... " << endl;
+
+    in >> C;
+    clients.resize(C);
+    for(auto & client : clients) {
+        client.read(in);
+    }
+
+    K = ingr_to_id.size();
+
 }
 
 void Greedy()
 {
     cerr << "Pot of Greed: draw 2 cards." << endl;
+
+    best.resize(K, 0);
+    PRNG rand(SEED);
+
+    for(auto & ingr : best) {
+        ingr = rand() % 2;
+    }
 }
 
 void PrintSolution(string filename)
@@ -96,6 +161,15 @@ void PrintSolution(string filename)
     ofstream out(filename);
     
     cerr << "Writing solution to file " << filename << " ... " << endl;
+
+    int used = accumulate(best.begin(), best.end(), 0);
+    out << used << " ";
+    for(int i=0; i<K; ++i) {
+        if(best[i]) {
+            out << id_to_ingr[i] << " ";
+        }
+    }
+    out << endl;
 }
 
 
